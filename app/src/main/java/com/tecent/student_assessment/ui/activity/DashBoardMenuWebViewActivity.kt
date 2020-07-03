a46395f4ed0952fecffc35b96d7a2c53d20b5e6a
Ajay Kumar <ajaykumar201761@gmail.com>
2020-07-03 15:44:49 +0530

    Adding onBackPresssed in Webview navigation to previous URL #1

--- app/src/main/java/com/tecent/student_assessment/ui/activity/DashBoardMenuWebViewActivity.kt
+++ app/src/main/java/com/tecent/student_assessment/ui/activity/DashBoardMenuWebViewActivity.kt
@@ -1,60 +1,81 @@
 package com.tecent.student_assessment.ui.activity
 
+import android.annotation.SuppressLint
 import android.graphics.drawable.ColorDrawable
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.tecent.student_assessment.R.drawable
 import com.tecent.student_assessment.R.id
 import com.tecent.student_assessment.R.layout
-import com.tecent.student_assessment.utils.ExtraFunctions
-import kotlinx.android.synthetic.main.activity_dash_board_menu_web_view.*
-import kotlinx.android.synthetic.main.toolbar_main.*
+import com.tecent.student_assessment.utils.ExtraFunctions.serverurl
+import kotlinx.android.synthetic.main.activity_dash_board_menu_web_view.dashboard_webview
+import kotlinx.android.synthetic.main.activity_dash_board_menu_web_view.progress_bar_dashboard
+import kotlinx.android.synthetic.main.toolbar_main.toolbar_main
 
 class DashBoardMenuWebViewActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(
-            layout.activity_dash_board_menu_web_view
-        )
-        setSupportActionBar(findViewById(
+  @SuppressLint("SetJavaScriptEnabled")
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(
+        layout.activity_dash_board_menu_web_view
+    )
+    setSupportActionBar(
+        findViewById(
             id.toolbar_main
-        ))
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        supportActionBar!!.setHomeAsUpIndicator(
-            drawable.ic_001_back
         )
-        supportActionBar!!.setBackgroundDrawable(ColorDrawable(-0x1))
-        dashboard_webview.getSettings().setJavaScriptEnabled(true)
-        dashboard_webview.getSettings().setBuiltInZoomControls(false)
-        val intentTitle = intent
-        val title: String = intentTitle.getStringExtra("title")
-        supportActionBar!!.setTitle(title.trim())
-        toolbar_main.setNavigationOnClickListener(View.OnClickListener {
-            finish()
-        })
-        dashboard_webview.webViewClient = object : WebViewClient() {
-            override fun onPageFinished(view: WebView?, url: String?) {
-                super.onPageFinished(view, url)
-                progress_bar_dashboard.visibility = View.GONE
-            }
-        }
-        when (title) {
-            "Prepare" -> dashboard_webview.loadUrl(
-                ExtraFunctions.serverurl + "dashboard/dashboardPrepare.php")
-            "Preparation Materials" -> dashboard_webview.loadUrl(
-                ExtraFunctions.serverurl + "dashboard/dashboardPrepareMaterials.php")
-            "Quizes" -> dashboard_webview.loadUrl(
-                ExtraFunctions.serverurl + "dashboard/dashboardQuizes.php")
-            "Tutorials" -> dashboard_webview.loadUrl("https://www.tutorials.a3creators.co.in")
-            "Useful Links" -> dashboard_webview.loadUrl(
-                ExtraFunctions.serverurl + "dashboard/usefulLinksDashboard.php")
-            "Practice" -> dashboard_webview.loadUrl(
-                ExtraFunctions.serverurl + "dashboard/Practice.php")
-        }
-
+    )
+    supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    supportActionBar!!.setHomeAsUpIndicator(
+        drawable.ic_001_back
+    )
+    supportActionBar!!.setBackgroundDrawable(ColorDrawable(-0x1))
+    dashboard_webview.settings.javaScriptEnabled = true
+    dashboard_webview.settings.builtInZoomControls = false
+    val intentTitle = intent
+    val title: String = intentTitle.getStringExtra("title")
+    supportActionBar!!.title = title.trim()
+    toolbar_main.setNavigationOnClickListener {
+      finish()
     }
+    dashboard_webview.webViewClient = object : WebViewClient() {
+      override fun onPageFinished(
+        view: WebView?,
+        url: String?
+      ) {
+        super.onPageFinished(view, url)
+        progress_bar_dashboard.visibility = View.GONE
+      }
+    }
+    when (title) {
+      "Prepare" -> dashboard_webview.loadUrl(
+          serverurl + "dashboard/dashboardPrepare.php"
+      )
+      "Preparation Materials" -> dashboard_webview.loadUrl(
+          serverurl + "dashboard/dashboardPrepareMaterials.php"
+      )
+      "Quizes" -> dashboard_webview.loadUrl(
+          serverurl + "dashboard/dashboardQuizes.php"
+      )
+      "Tutorials" -> dashboard_webview.loadUrl("https://www.tutorials.a3creators.co.in")
+      "Useful Links" -> dashboard_webview.loadUrl(
+          serverurl + "dashboard/usefulLinksDashboard.php"
+      )
+      "Practice" -> dashboard_webview.loadUrl(
+          serverurl + "dashboard/Practice.php"
+      )
+    }
+
+  }
+
+  override fun onBackPressed() {
+    if (dashboard_webview.canGoBack()) {
+      dashboard_webview.goBack()
+    } else {
+      super.onBackPressed()
+    }
+  }
 }
--- app/src/main/java/com/tecent/student_assessment/ui/activity/ImageViewerActivity.kt
+++ app/src/main/java/com/tecent/student_assessment/ui/activity/ImageViewerActivity.kt
@@ -1,45 +1,44 @@
 package com.tecent.student_assessment.ui.activity
 
 import android.content.pm.ActivityInfo
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
-import android.view.WindowManager
-import kotlinx.android.synthetic.main.activity_image_viewer.*
-import uk.co.senab.photoview.PhotoViewAttacher
-import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
 import com.tecent.student_assessment.R.layout
+import kotlinx.android.synthetic.main.activity_image_viewer.imageView
+import uk.co.senab.photoview.PhotoViewAttacher
 
 class ImageViewerActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(
-            layout.activity_image_viewer
-        )
-        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT  //Prevent landscape mode
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(
+        layout.activity_image_viewer
+    )
+    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT  //Prevent landscape mode
 
-        changeStatusBarColor()
-        val intentType=intent.getStringExtra("intentType")
-        when(intentType){
-            "byteArray"->{
-                val byteArray=intent.getByteArrayExtra("imageByteArray")
-                val bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
-                imageView.setImageBitmap(bmp)
-            }
-        }
-
-        val pAttacher = PhotoViewAttacher(imageView)
-        pAttacher.update()
+    changeStatusBarColor()
+    when (intent.getStringExtra("intentType")) {
+      "byteArray" -> {
+        val byteArray = intent.getByteArrayExtra("imageByteArray")
+        val bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+        imageView.setImageBitmap(bmp)
+      }
     }
 
+    val pAttacher = PhotoViewAttacher(imageView)
+    pAttacher.update()
+  }
 
-    private fun changeStatusBarColor() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            val window = window
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            window.statusBarColor = Color.BLACK
-        }
+  private fun changeStatusBarColor() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      val window = window
+      window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+      window.statusBarColor = Color.BLACK
     }
+  }
+
 }
